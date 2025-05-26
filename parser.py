8b74510263d077d01a077196ca4d8b50a4bce531
akatsukiHell <134585973+akatsukiHell@users.noreply.github.com>
2025-05-26 11:47:47 +0300

    Initial coomit

---
+++ parser.py
@@ -0,0 +1,17 @@
+from typing import Optional
+import requests
+
+article = input(str("Введите артикул: "))
+
+def get_wb_info(article: str) -> Optional[dict]:
+    url = f"https://card.wb.ru/cards/detail?&dest=-1257786&nm={article}"
+    response = requests.get(url)
+    if response.status_code != 200:
+        return None
+    else:
+        data = response.json()['data']['products'][0]
+        return {data['name']: data['priceU'] // 100}
+
+
+if __name__ == "__main__":
+    print(get_wb_info(article))
